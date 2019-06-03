8116f3bafb3473b3bc1c2d0d4525250aae636271
jonathanglennwomack <jonathanglennwomack@gmail.com>
2019-06-03 11:26:30 -0400

    Changed port

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ var http = require('http'), fs = require('fs');
 var url = require('url');
 var path = require('path');
 var express = require('express');
+var port = process.env.PORT || 3000;
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -11,7 +12,7 @@ app.get('/home', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.listen(port);
 
 /*
 var retext = require('retext');
